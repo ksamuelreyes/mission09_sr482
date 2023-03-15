1478b71c6a500e61f26266e1cee2651e5f112a17
Sam Reyes <ksamuelreyes@gmail.com>
2023-03-14 21:21:50 -0600

    Added cart icon and description / Commented code

---
+++ mission09_sr482/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mission09_sr482.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace mission09_sr482.Components 
+{ 
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket cart;
+        public CartSummaryViewComponent(Basket cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
